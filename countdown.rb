da94fdc3afb598c9840ba385324bc234c5a5f8bf
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-29 01:32:08 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,9 @@
 
 def countdown(number)
   while number > 0
-  puts "#{number} SECOND(S)!"
-  number-=1
-end
+    puts "#{number} SECOND(S)!"
+    number-=1
+  end
   return "HAPPY NEW YEAR!"
 end
 
